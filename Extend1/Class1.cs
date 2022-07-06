fdba034fa961040e993724bd1734568cb71c962c
BirktiAdu <desalegnbiruktawit9487@gmail.com>
2022-07-06 11:44:25 +0300

    First Version

---
+++ Extend1/Class1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extend1
+{
+    internal class Class1
+    {
+    }
+}
+
+    abstract class Ishape
+    {
+        abstract public double calculateArea();
+        abstract public double calculateperimeter();
+    }
+
+
+    class Circle : Ishape
+    {
+
+        public double radius { get; set; }
+        public double area { get; set; }
+        public static double PI = 3.14;
+        public double perimeter { get; set; }
+        public override double calculateArea()
+        {
+            area = PI * radius * radius;
+            return area;
+        }
+
+        public override double calculateperimeter()
+        {
+            perimeter = 2 * radius * PI;
+            return perimeter;
+        }
+    }
+
+    class Rectangle : Ishape
+    {
+        public double perimeter { get; set; }
+        public double area { get; set; }
+
+        public double length { get; set; }
+        public double width { get; set; }
+        public override double calculateArea()
+        {
+            area = length * width;
+            return area;
+        }
+
+        public override double calculateperimeter()
+        {
+            perimeter = 2 * (length * width);
+            return perimeter;
+        }
+    }
---
+++ Extend1/Program.cs
@@ -0,0 +1,67 @@
+﻿
+
+    
+        int n;
+        Circle c = new Circle();
+        Rectangle r = new Rectangle();
+        char res;
+        do
+        {
+            Console.WriteLine("\t\t1. Calculate Area for Circle");
+            Console.WriteLine("\t\t2. Calculate Perimeter for Circle");
+            Console.WriteLine("\t\t3. Calculate Area for Rectangle");
+            Console.WriteLine("\t\t4. Calculate Pereimeter for Rectangle");
+            n = int.Parse(Console.ReadLine());
+
+    if (n == 1)
+
+    {
+        Console.WriteLine("Enter radius: ");
+        c.radius = Convert.ToDouble(Console.ReadLine());
+        c.calculateArea();
+        Console.WriteLine("Area= " + c.area);
+    }
+
+    else if (n == 2)
+    {
+        Console.WriteLine("Enter radius: ");
+        c.radius = Convert.ToDouble(Console.ReadLine());
+        c.calculateperimeter();
+        Console.WriteLine("Perimeter= " + c.perimeter);
+    }
+    else if (n == 3)
+    {
+        Console.WriteLine("Enter length: ");
+        r.length = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Enter width: ");
+        r.width = Convert.ToDouble(Console.ReadLine());
+        r.calculateArea();
+        Console.WriteLine("Area= " + r.area);
+    }
+    else if (n == 4)
+    {
+        Console.WriteLine("Enter length: ");
+        r.length = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Enter width: ");
+        r.width = Convert.ToDouble(Console.ReadLine());
+        r.calculateperimeter();
+        Console.WriteLine("Perimeter= " + c.perimeter);
+    }
+    else
+    {
+
+
+        { Console.WriteLine("!!!"); }
+
+    }
+            Console.WriteLine("Do you want more? ('Y' for yes and 'N' for no'");
+            res = Convert.ToChar(Console.ReadLine());
+        } while (res == 'Y');
+
+
+
+
+
+    
+
+
